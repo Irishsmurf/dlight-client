6fab944ee0e00941ab4f96e993f2ea36e61d70a3
David Kernan <dave@paddez.com>
2025-04-12 06:21:32 -0700

    WiFi

--- dlightclient/__init__.py
+++ dlightclient/__init__.py
@@ -1,4 +1,4 @@
 # dlightclient/__init__.py
 from .dlight import AsyncDLightClient, DLightError, DLightConnectionError, DLightTimeoutError, DLightCommandError, DLightResponseError
 
-__version__ = "1.0.0" # Define package version here
+__version__ = "1.0.1" # Define package version here
